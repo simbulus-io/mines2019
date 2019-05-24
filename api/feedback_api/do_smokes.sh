e2bd761cc265564ca756beb3605ace8feb852a3e
Sean Kelly <sean.kelly@simbulus.com>
2019-05-24 14:03:46 -0600

    smoke for feedback team

--- api/feedback_api/do_smokes.sh
+++ api/feedback_api/do_smokes.sh
@@ -6,5 +6,5 @@ export HTML_OUTPUT="$OUTPUT_PATH"/smokes_branch_"$BRANCH_NAME".html
 export JSON_OUTPUT="$OUTPUT_PATH"/smokes_branch_"$BRANCH_NAME".json
 export JEST_HTML_REPORTER_OUTPUT_PATH=$HTML_OUTPUT
 export JEST_HTML_REPORTER_PAGE_TITLE=Smoke\ Tests\ \-\-\ "$BRANCH_NAME"\ branch
-yarn test test/*.smoke.ts --silent --runInBand --json --outputFile $JSON_OUTPUT
+yarn test test/*.smoke.ts --forceExit --runInBand --json --outputFile $JSON_OUTPUT
 true
--- api/feedback_api/test/mongo.smoke.ts
+++ api/feedback_api/test/mongo.smoke.ts
@@ -1,22 +1,43 @@
 import * as _                      from 'lodash';
 import { MongoHelper }             from '../src/helpers/mongo_helper';
 import { logger }                  from './logger_helper';
+import { MongoClient }             from 'mongodb';
+import { exitOnError } from 'winston';
 
 const randomname = require('random-name');
 
-describe('MongoHelper smoke tests', () => {
+let mongo: MongoClient;
 
-  test('TestWrite', async () => {
-    const mongo = await MongoHelper.connect();
-    const collection = mongo.db('feedback').collection('test_collection');
-    for (let i = 0; i < 10; ++i) {
-      const data = {idx: i, name: randomname.first() };
-      const rval = await collection.updateOne(
-        { idx: i},        // insert index
-        { $set: data },   // write concern (data we are updating)
-        { upsert: true }, // upsert means create document if it does not exist
-      );
+beforeAll(async () => {
+  mongo = await MongoHelper.connect();
+});
+
+afterAll(async () => {
+  logger.info('afterAll');
+});
+
+describe('MongoHelper smoke tests', () => {
+    test('TestWrite', async () => {
+    // Here we create or update 10 documents in the feedback database in the test_collection
+
+    try {
+      const collection = mongo.db('feedback').collection('test_collection');
+      const promises: Array<Promise<any>> = [];
+      for (let i = 0; i < 10; ++i) {
+        const data = {idx: i, name: randomname.first() };
+        const promise = collection.updateOne(
+          { idx: i},        // insert index
+          { $set: data },   // write concern (data we are updating)
+          { upsert: true }, // upsert means create document if it does not exist
+        );
+        promises.push(promise);
+      }
+      const rval = await Promise.all(promises);
       logger.info(rval);
+      logger.info('Done');
+      return;
+    } catch (e) {
+      logger.error('Unexpected Exception TestWrite', e);
     }
   });
 
