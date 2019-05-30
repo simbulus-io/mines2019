a61ad3be5b9fcfce165cee6b1657ae2fd0a5f64a
Sean Kelly <sean.kelly@simbulus.com>
2019-05-30 15:40:52 -0600

    for AP

--- app/feedback/ignore/src/main.js
+++
@@ -1,4 +0,0 @@
-// main for production build -- assumes $hxClasses is loaded
-import { load_app } from './load_app';
-load_app();
-//# sourceMappingURL=main.js.map
--- app/feedback/ignore/src/serve.js
+++
@@ -1,3 +0,0 @@
-import { load_app } from './load_app';
-load_app();
-//# sourceMappingURL=serve.js.map
