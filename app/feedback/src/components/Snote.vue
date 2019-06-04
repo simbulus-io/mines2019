f1676af4b17d8c6ed6796ecbaec79bf7c44ddccd
alexrpollock <arpollock@mines.edu>
2019-06-04 16:09:17 -0600

    Save and close button to make sticky note unselected instead of clicking away (now stretch goal). Used Vue.set for reactivity. Saving message change still needs to be implemented

--- app/feedback/src/components/Snote.vue
+++ app/feedback/src/components/Snote.vue
@@ -1,21 +1,25 @@
 <template>
 
-  <!-- TODO: have selected turn to false when click off 
+  <!-- TODO: have selected turn to false when click off 6/4 - this might not be needed bc of save and close button (now stretch goal)
     maybe:
       https://github.com/ndelvalle/v-click-outside
       https://github.com/simplesmiler/vue-clickaway // trying this one 5/31
   -->
-  <div class="snote-inactive" v-bind:class="{ 'snote-active': selected }" v-show="!get_note.deleted" v-on:click="selected=true" v-click-outside="onClickOutside" v-bind:style="{top:get_note.y+'px', left:get_note.x+'px'}"> <!-- always has class snote-inactive, snote-active is also applied when selected is true -->
-    <div class="delete-wrapper" v-show="selected">
-      <button class="delete" v-on:click="delete_snote"><font-awesome-icon icon="trash" color="rgba(235, 77, 75,1.0)"/></button>
-	  </div>
+  
+  <div class="snote-inactive" v-bind:class="{ 'snote-active': selected }" v-show="!get_note.deleted" v-on:click="selected=true" v-bind:style="{top:get_note.y+'px', left:get_note.x+'px'}"> <!-- always has class snote-inactive, snote-active is also applied when selected is true -->
+    <div class="button-wrapper" v-show="selected">
+      <button class="snote-button" v-on:click="delete_snote"><font-awesome-icon icon="trash" color="rgba(235, 77, 75,1.0)"/></button>
+    </div>
     <!-- TODO: have content update when exit out of note. -->
     <!-- TODO: have textarea be size of content (currently set at 5) -->
     <textarea class="snote-input" v-model="get_note.content" rows="5" v-bind:readonly="!selected"></textarea>
     <p class="snote-text attr">Written by {{ get_note.author }}</p>
     <p class="snote-text">{{ get_date }}</p>
+    <div class="button-wrapper" v-show="selected">
+      <button class="snote-button" v-on:click="save_exit_snote" @click.stop>Save &amp; Close Note</button>
+      <!-- @click.stop needed to keep Snote click listener from changing selected right back to true -->
+    </div>
   </div>
-  
 
 </template>
 
@@ -27,7 +31,7 @@
   background-color: inherit;
   border: none;
 }
-.delete {
+.snote-button {
 	float: right;
   margin-bottom: 10px;
 }
