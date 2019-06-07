3ec27272b972f5618ba277d252f916d2d2637fa0
guacamole777 <earellanomendiola@mymail.mines.edu>
2019-06-07 11:24:19 -0600

    Added button to go from Teacher Slug View to Teacher Assignment View

--- app/feedback/src/components/TeacherAssignment.vue
+++ app/feedback/src/components/TeacherAssignment.vue
@@ -9,6 +9,8 @@
         <!-- Links to the symbols for the toolbar buttons -->
       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+     <router-link to="/teacher/slug"><button class="button" title="Return to All Assignments">Return to All Assignments</button></router-link>
+
       <div class = "toolbar">
        <button-toolbar  key-nav aria-label="Toolbar with button groups" >
         <button class="button" title="Add Comment" v-on:click="create_snote"><i class ="fa fa-plus" ></i></button>
