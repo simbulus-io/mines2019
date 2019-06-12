af2cc550856021f96dca9f407a4454a84857f0dd
alexrpollock <arpollock@mines.edu>
2019-06-12 13:53:42 -0600

    Fixing error of student snotes with absolute positioning

--- app/feedback/src/components/SnoteStudent.vue
+++ app/feedback/src/components/SnoteStudent.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="snote-inactive" v-show="!get_note.deleted" v-bind:style="{top:get_note.y+'px', left:get_note.x+'px'}"> <!-- always has class snote-inactive, snote-active is also applied when selected is true -->
+  <div class="snote-inactive snote-wrapper" v-show="!get_note.deleted" v-bind:style="{top:get_note.y+'px', left:get_note.x+'px'}"> <!-- always has class snote-inactive, snote-active is also applied when selected is true -->
     <textarea class="snote-input" v-model="get_note.content" rows="5" readonly></textarea>
     <p class="snote-text attr">Written by {{ get_note.author }}</p>
     <p class="snote-text">{{ get_date }}</p>
