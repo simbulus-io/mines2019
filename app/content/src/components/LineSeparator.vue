7463219ded4aefdd68be3d4612472f35905a3b97
Dante Arcuri <dantearcuri@mymail.mines.edu>
2019-06-12 11:16:33 -0600

    WIP:Added button to add line

--- app/content/src/components/LineSeparator.vue
+++ app/content/src/components/LineSeparator.vue
@@ -3,8 +3,8 @@
 
   <div class="line_separator"  style="position: relative;">
     <!--Displays vertical offset of first horizontal segmentation line-->
-    <button type="button" onclick="alert('Hello')">Add Another line</button>
-
+    <button type="button" onclick="text_in_div">Add Another line</button>    
+    
     <drag-it-dude@activated="handleActivated" @dragging="handleDragging" @dropped="handleDropped"></drag-it-dude>
     <!--Adds Image to page-->
     <img src='./EngageNY/Algebra1-m03-b-l08.png'>
@@ -18,18 +18,21 @@
       
       </drag-it-dude>
 
-
+      
       <drag-it-dude@activated="handleActivated" @dragging="handleDragging" @dropped="handleDropped">
       <div class="line" align="center" onmouseup="console.log(getBoundingClientRect().top -63 + window.pageYOffset)">
         <hr size="5px" width="99%" color="red">
       </div>
       </drag-it-dude>
-
+      
       <drag-it-dude @activated="handleActivated" @dragging="handleDragging" @dropped="handleDropped">
+      
       <div class="line" align="center" onmouseup="console.log(getBoundingClientRect().top -63 + window.pageYOffset)">
         <hr size="5px" width="99%" color="red">
       </div>
       </drag-it-dude>
+  <div class="Button">
+    </div>
   </div>
 </template>
 
@@ -44,6 +47,7 @@
   width: 825px;
   height: 5250px;
 } 
+
 .line {
   top: 15px;
   position: relative;
