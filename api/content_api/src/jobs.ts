6eb80344dec16b99b23c319a78a9d8282763b038
Sean Kelly <sean.kelly@simbulus.com>
2019-06-17 09:30:30 -0600

    jobs schema with optional implemented correctly

--- api/content_api/src/jobs.ts
+++ api/content_api/src/jobs.ts
@@ -1,21 +1,23 @@
   // tslint:disable: variable-name
 import * as t         from 'io-ts';
+import { tsAnyKeyword } from '@babel/types';
 
 // JobInput Schema
 
-// The syntax for combining objects with required and optional keys is
-// complicated ... we'll end up punting this lib or using a helper
-// so keep in mind that the below simply states that the values
-// can be undefined not that the keys are missing from the payload
-
-export const JobInput = t.type({
-  command: t.string,                            // required. See .../python-coproc/app.py for list
-  args:    t.union([t.object , t.undefined]),   // optional but commands usually require specific args
-  name:    t.union([t.string , t.undefined]),   // optional: job name (job_id guid added regardless)
-  dir:     t.union([t.string , t.undefined]),   // optional: directory to create and set as working dir.
-                                                // if not set, dir defaults to (name || job_id)
-  //timeout: t.union([t.string , t.undefined]), // optional: in seconds. NOT YET IMPLEMENTED
+export const JobInputOptional = t.partial({
+  timeout: t.string,
+  args:    t.object,   // optional but commands usually require specific args
+  name:    t.string,   // optional: job name (job_id guid added regardless)
+  dir:     t.string,   // optional: create and set as working dir - defaults to (name || job_id)
 });
+
+export const JobInputRequired = t.type({
+  command: t.string,   // required. See .../python-coproc/app.py for list
+});
+
+// This is the syntax for creating an object with combined optional and required
+// keys
+export const JobInput = t.intersection([JobInputRequired, JobInputRequired]);
 
 // JobID Schema
 export const JobID = t.type({
