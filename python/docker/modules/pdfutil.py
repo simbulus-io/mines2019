1d66dce0271ae81b26cbb0ebbc30fd4dab533888
Brent Milne <brent.milne@nimbee.com>
2019-06-21 17:29:06 -0600

    pdfminer example

--- python/docker/modules/pdfutil.py
+++ python/docker/modules/pdfutil.py
@@ -3,6 +3,7 @@ import cv2
 import io
 import numpy as np
 from PyPDF2 import PdfFileWriter, PdfFileReader
+import subprocess
 from wand.image import Image
 from wand.color import Color
 
@@ -36,3 +37,59 @@ def strings_from_page(page):
     strings = page.extractText().split('\n')
     strings = [s for s in strings if s.strip()!=""]
     return strings
+
+
+import xml.etree.ElementTree as ET 
+import yaml
+
+def s2vec(s,delim=','):
+    return [float(w) for w in s.split(delim)]
+
+def fontspec(attr, incl_mods=False):
+    if not 'font' in attr:
+        return None
+    f = attr['font']
+    sz = float(attr['size'])
+    is_bold = 'bold' in f.lower()
+    is_ital = 'italic' in f.lower()
+    f = f.split(',')[0]
+    f = f.split('+')[-1]
+    f = f.replace('Bold','').replace('Italic','').strip()
+    desc = [f, '%d'%round(sz)]
+    if incl_mods:
+        if is_bold:  desc.push('Bold')
+        if is_ital:  desc.push('Ital')
+    return ';'.join(desc)
+    
+
+def mine_strings(filename):
+
+    cmd = "pdf2txt.py '%s' -t xml" % filename
+    d = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE).stdout.read()
+    pages = ET.fromstring(d)
+    # tree = ET.parse('foo.xml')
+    # pages = tree.getroot()
+
+    desc = []
+    for pg in pages.findall('./page'):
+        pg_desc = {}
+        pg_desc['bbox'] = s2vec(pg.attrib['bbox'])
+        strings = []
+        for bx in pg.findall('./textbox'):
+            for ln in bx.findall('./textline'):
+                # bbox = print(ln.attrib['bbox'])
+                bbox = s2vec(ln.attrib['bbox'])
+                text = ''
+                fonts = set()
+                for t in ln.findall('./text'):
+                    atr = t.attrib
+                    text += t.text
+                    fs = fontspec(atr)
+                    if not fs is None:
+                        fonts.add(fs)
+                if text.strip()!='':
+                    strings.append({'bbox': bbox, 'text': text.strip(), 'fonts': list(fonts)})
+        pg_desc['strings'] = strings
+        desc.append(pg_desc)
+
+    return desc
