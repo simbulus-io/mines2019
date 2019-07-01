c79aef5b48be63a53d274e22e7a6f76e83fd0220
Alex Pollock <alex.pollock@wootmath.com>
2019-07-01 11:06:15 -0600

    Removing unnecessary code/comments

--- app/content/src/components/dashboard.ts
+++ app/content/src/components/dashboard.ts
@@ -21,59 +21,11 @@ export default class Dashboard extends Vue {
   }
 
   public currSelection!: any;
-  
+
+  // Computed
   public get content_data() {
     log.info('Grabbing content_tree data from store');
     return this.$store.state.content.content_tree;
-    // return {
-    //     'EngageNY':{
-    //       'Grade':{
-    //         'Kindergarten':'No Content',
-    //         'First':{
-    //           'Module 1':{
-    //             'Lesson 1':'Addition',
-    //             'Lesson 2':'Subtraction',
-    //             'Lesson 3':'Advanced Quadratic Equations',
-    //             'Misc':{
-    //               'Graphing':
-    //                 ['Stuff','Even Cooler Stuff']
-    //             },
-    //             'Module 2':'No Content'
-    //           }
-    //         },
-    //         'Second':'No Content',
-    //       }
-    //     },
-    //     'Other Content Source': 'TBD'
-    //   }
-  }
-
-  // public data = {
-  //   'EngageNY':{
-  //     'Grade':{
-  //       'Kindergarten':'No Content',
-  //       'First':{
-  //         'Module 1':{
-  //           'Lesson 1':'Addition',
-  //           'Lesson 2':'Subtraction',
-  //           'Lesson 3':'Advanced Quadratic Equations',
-  //           'Misc':{
-  //             'Graphing':
-  //               ['Stuff','Even Cooler Stuff']
-  //           },
-  //           'Module 2':'No Content'
-  //         }
-  //       },
-  //       'Second':'No Content',
-  //     }
-  //   },
-  //   'Other Content Source': 'TBD'
-  // }
-  
-
-  // Computed
-  public get hello_mines() {
-    return this.$store.state.content.test_data;
   }
 
   public itemSelected(e: any){ // when a leaf node is selected
