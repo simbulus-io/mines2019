4ed98cc3eeca9de3b008b92832af690312b992fe
Brent Milne <brent.milne@nimbee.com>
2019-07-10 09:41:56 -0600

    elaborating templates from root of repo; example of elaborating python settings via ENV

--- build.rb
+++ build.rb
@@ -4,30 +4,36 @@ require 'erubis'
 require 'fileutils'
 require 'socket'
 
+Erdos = '10.0.1.3'
+Local_Network = /^10\./
+
 def main(opts)
   script_dir = File.expand_path(__dir__)
+  root_dir = script_dir
 
   Dir.chdir(script_dir)
 
   my_ip_address = ''
   Socket.ip_address_list.each do |addr_info|
     addr = addr_info.ip_address
-    my_ip_address = addr if addr =~ /^10\./
+    my_ip_address = addr if addr =~ Local_Network
   end
   raise "couldn't figure out my ip address" if my_ip_address.length==0
 
-  for_production = opts.prod
-  nginx_port = opts.port
-  file_share = opts.share
-  
-  templates = %w( nginx/nginx.conf.erb  docker-compose.yml.erb )
+  # Each file in templates[] will get erb-expanded into a file of the same name (minus the trailing .erb)
+  # Paths are relative to root_dir
+  templates = %w( 
+    stack/docker/nginx/nginx.conf.erb  
+    stack/docker/docker-compose.yml.erb 
+    python/python-coproc/export_env.erb
+  )
 
   templates.each do |tmplt|
-    src = script_dir+"/"+tmplt
-    tgt = src.sub('.erb','')
+    src = root_dir+"/"+tmplt
+    tgt = src.sub(/\.erb$/,'')
     erb = File.read(src)
     result = Erubis::Eruby.new(erb).result(binding())
-    File.open(tgt, "w") { |f| f.puts(result) }
+    File.open(tgt, "w") { |f| f.puts(result); puts("Wrote #{tgt}");}
   end
 
 end
@@ -39,10 +45,15 @@ if __FILE__==$0
   require 'optparse'
   require 'ostruct'
 
-  opts = OpenStruct.new
-  opts[:port] = '80'
-  opts[:share] = "#{ENV['HOME']}/shared"
   
+  opts = OpenStruct.new
+  # Defaults:
+  opts[:port]          = '80'
+  opts[:share]         = "#{ENV['HOME']}/shared"
+  opts[:mongo_url]     = "mongodb://#{Erdos}:27017"
+  opts[:mongo_db_name] = "internal_tools_gester"
+
+  # Process commandline options:
   OptionParser.new do |parser|
     parser.on('--prod',
               'serve app/dist rather than webpack dev_server'
